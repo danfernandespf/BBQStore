b859f76be5d03917c9c9694d271a2a2879e6615b
Daniel de Pinho Fernandes <daniel.fernandes@sbcredito.com.br>
2021-02-23 17:56:48 -0300

    Identity

---
+++ src/services/BBQStore.Identity.API/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using BBQStore.Identity.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BBQStore.Identity.API.Controllers
+{
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser 
+            { 
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(
+                usuarioLogin.Email, 
+                usuarioLogin.Senha, 
+                isPersistent: false, // Salvar estado do usuário logado.
+                lockoutOnFailure: true); // Bloquear usuário após muitos erros no login.
+
+            if (result.Succeeded) return Ok();
+
+            return BadRequest();
+        }
+    }
+}
